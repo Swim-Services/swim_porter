66ef0b4fa5ca4838d4bd4d8c9246507f6b244f7f
GameParrot <85067619+GameParrot@users.noreply.github.com>
2024-03-11 07:48:37 -0400

    fix newline

--- port/utils/mcmeta.go
+++ port/utils/mcmeta.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"bytes"
 	"encoding/json"
+	"regexp"
+	"strings"
 )
 
 type PackMeta struct {
@@ -13,11 +15,21 @@ type PackMeta struct {
 }
 
 func PackMcmeta(mcmeta []byte) (PackMeta, error) {
-	mcmeta = bytes.TrimPrefix(mcmeta, []byte("\xef\xbb\xbf"))
+	mcmeta = []byte(fix(strings.ReplaceAll(string(bytes.TrimPrefix(mcmeta, []byte("\xef\xbb\xbf"))), "\r", "")))
 	meta := PackMeta{}
 	err := json.Unmarshal(mcmeta, &meta)
 	if err != nil {
 		return PackMeta{}, err
 	}
 	return meta, nil
+}
+
+var octalEscapePat = regexp.MustCompile(`\\[0-7]{3}`)
+var newlineEscape = regexp.MustCompile(`"(?:[^"\\]|\\.)*"`)
+
+func fix(src string) string {
+	src = newlineEscape.ReplaceAllStringFunc(src, func(s string) string {
+		return strings.ReplaceAll(s, "\n", "\\n")
+	})
+	return octalEscapePat.ReplaceAllString(src, " ")
 }
